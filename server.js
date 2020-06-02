a95263555efb970300ac331a4c0b0ae64c9f071b
Jared Tyler <jared@jaredtyler.ca>
2020-06-02 15:56:19 -0400

    If approaching a wall and you encounter your body, seek out your own tail - that spot should be safe by the time you get there.

--- server.js
+++ server.js
@@ -13,6 +13,33 @@ app.get('/', (req, res) => {
 	});
 });
 
+let target = null;
+const approachTarget = (you, target) => {
+	console.log('You:', you.head);
+	console.log('Target:', target);
+	
+	const diffX = target.x - you.head.x;
+	const diffY = target.y - you.head.y;
+	
+	const absX = Math.abs(diffX);
+	const absY = Math.abs(diffY);
+	
+	console.log('DiffX:', diffX, ';  DiffY:', diffY);
+	const moveX = Math.sign(diffX) + you.head.x;
+	const moveY = Math.sign(diffY) + you.head.y;
+	if (moveX != you.body[1].x) {
+		if (absX >= absY || moveY == you.body[1].y) {
+			const move = Math.sign(diffX) < 0 ? 'left' : 'right';
+			console.log('Moving X:', move);
+			return move;
+		}
+	}
+	
+	const move = Math.sign(diffY) < 0 ? 'down' : 'up';
+	console.log('Moving Y:', move);
+	return move;
+};
+
 let maxTravel = 0;
 app.post('/start', (req, res) => {
 	const { game, board, you } = req.body;
@@ -27,30 +54,17 @@ let move = 'right';
 app.post('/move', (req, res) => {
 	const { game, board, you } = req.body;
 	
-	if (you.health <= maxTravel) {
-		const target = board.food[0];
-		console.log('You:', you.head);
-		console.log('Target:', target);
-		
-		const diffX = target.x - you.head.x;
-		const diffY = target.y - you.head.y;
-		
-		const absX = Math.abs(diffX);
-		const absY = Math.abs(diffY);
-		
-		console.log('DiffX:', diffX, ';  DiffY:', diffY);
-		const moveX = Math.sign(diffX) + you.head.x;
-		const moveY = Math.sign(diffY) + you.head.y;
-		if (moveX != you.body[1].x) {
-			if (absX >= absY || moveY == you.body[1].y) {
-				move = Math.sign(diffX) < 0 ? 'left' : 'right';
-				console.log('Moving X:', move);
-				return res.send({ move });
-			}
+	if (target) {
+		if (you.head.x == target.x && you.head.y == target.y) target = null;
+		else {
+			move = approachTarget(you, target);
+			return res.send({ move });
 		}
-		
-		move = Math.sign(diffY) < 0 ? 'down' : 'up';
-		console.log('Moving Y:', move);
+	}
+	
+	if (you.health <= maxTravel) {
+		target = board.food[0];
+		move = approachTarget(you, target);
 		return res.send({ move });
 	}
 	
@@ -62,6 +76,23 @@ app.post('/move', (req, res) => {
 	};
 	
 	if (engine[move].limit) move = engine[move].next;
+	
+	let x = you.head.x;
+	let y = you.head.y;
+	if (move == 'up') y += 1;
+	if (move == 'down') y -= 1;
+	if (move == 'right') x += 1;
+	if (move == 'left') x -= 1;
+	
+	const collides = you.body.reduce((hit, coords) => {
+		return hit || (x == coords.x && y == coords.y);
+	}, false);
+	
+	if (collides) {
+		target = you.body[you.body.length - 1];
+		move = approachTarget(you, target);
+		return res.send({ move });
+	}
 	
 	return res.send({ move });
 });
