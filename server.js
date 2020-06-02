ba77ce716c13d084a47996f3e344f3a1032baa4e
Jared Tyler <jared@jaredtyler.ca>
2020-06-02 13:08:11 -0400

    A technically functioning battlesnake.

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 
 const app = express();
+app.use(require('body-parser').json());
 
 app.get('/', (req, res) => {
-	console.log('bleep');
-	res.send('bloop');
+	res.send({
+		apiversion: '1',
+		author: 'JMTyler',
+		color: '#660066',
+		head: 'default',
+		tail: 'default',
+	});
 });
 
-app.listen(process.env.PORT || 9000);
+app.post('/start', (req, res) => {
+	console.log(`New Game! [${req.body.game.id}]`)
+	res.sendStatus(200);
+});
+
+app.post('/move', (req, res) => {
+	res.send({ move: 'right' });
+});
+
+app.post('/end', (req, res) => {
+	console.log('GAMEOVER', req.body);
+	console.log('-----\n');
+	res.sendStatus(200);
+});
+
+app.listen(process.env.PORT || 9000, () => console.log('Running!'));
