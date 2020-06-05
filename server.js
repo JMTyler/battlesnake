5bba24a4d8bc67b8ec98e9208152b2c18cf52478
Jared Tyler <jared@jaredtyler.ca>
2020-06-05 15:06:44 -0400

    Cleanup.

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ app.use(require('body-parser').json());
 app.get('/', (req, res) => {
 	res.send({
 		apiversion: '1',
-		author: 'JMTyler',
-		color: '#700070',
-		head: 'beluga',
-		tail: 'shac-coffee',
+		author:     'JMTyler',
+		color:      '#700070',
+		head:       'beluga',
+		tail:       'shac-coffee',
 	});
 });
 
@@ -25,33 +25,29 @@ const State = {
 
 const Path = {
 	ApproachTarget(target, { board, you }) {
-		console.log('You:', you.head);
-		console.log('Target:', target);
-		
 		const vector = Path.GetVector(you.head, target);
 		const adjacent = Position.GetAdjacentTiles(you.head);
-		
+
 		const moveX = adjacent[vector.dir.x];
 		if (!Position.IsSafe(moveX, { board, you })) {
 			return vector.dir.y;
 		}
-		
+
 		const moveY = adjacent[vector.dir.y];
 		if (!Position.IsSafe(moveY, { board, you })) {
 			return vector.dir.x;
 		}
-		
+
 		if (you.head.x != target.x) {
 			return vector.dir.x;
 		}
-		
+
 		return vector.dir.y;
 	},
-	
+
 	GetVector(origin, dest) {
 		const x = dest.x - origin.x;
 		const y = dest.y - origin.y;
-		console.log('DiffX:', x, ';  DiffY:', y);
 		return {
 			dir: {
 				x: Math.sign(x) < 0 ? 'left' : 'right',
@@ -60,12 +56,12 @@ const Path = {
 			weight: { x, y },
 		};
 	},
-	
+
 	GetDistance(origin, target) {
 		if (_.isArray(target)) {
 			return _.map(target, (t) => Path.GetDistance(origin, t));
 		}
-		
+
 		const x = target.x - origin.x;
 		const y = target.y - origin.y;
 		return Math.hypot(x, y);
@@ -81,16 +77,16 @@ const Position = {
 			right: { x: x + 1, y },
 		};
 	},
-	
+
 	IsOutsideBoard({ x, y }, board) {
 		return x < 0 || y < 0 || x >= board.width || y >= board.height;
 	},
-	
+
 	IsDeadly(pos, { board }) {
 		if (Position.IsOutsideBoard(pos, board)) {
 			return true;
 		}
-		
+
 		const anySnakeCollision = _.some(board.snakes, (snake) => {
 			// TODO: Only drop the tail piece if the snake HASN'T just eaten a disc.
 			// TODO: Or, drop the tail either way, but consider that spot risky.
@@ -99,10 +95,10 @@ const Position = {
 		if (anySnakeCollision) {
 			return true;
 		}
-		
+
 		return false;
 	},
-	
+
 	IsRisky(pos, { board, you }) {
 		return _.some(board.snakes, (snake) => {
 			if (snake.id === you.id) return false;
@@ -110,7 +106,7 @@ const Position = {
 			return _.some(adjacent, pos);
 		});
 	},
-	
+
 	IsSafe(pos, context) {
 		return !Position.IsDeadly(pos, context) && !Position.IsRisky(pos, context);
 	},
@@ -127,58 +123,56 @@ const Food = {
 };
 
 app.post('/start', (req, res) => {
-	const { game, board, you } = req.body;
-	
-	console.log(`New Game! [${game.id}]`);
+	const { board } = req.body;
+
 	State.Set(req.body, {
-		maxTravel : board.width + board.height - 2,
-		turns     : 0,
-		target    : null,
-		move      : 'right',
+		maxTravel: board.width + board.height - 2,
+		turns:     0,
+		target:    null,
+		move:      'right',
 	});
-	
+
 	return res.sendStatus(200);
 });
 
 app.post('/move', (req, res) => {
-	const { game, board, you } = req.body;
+	const { board, you } = req.body;
 	const state = State.Get(req.body);
 	const adjacent = Position.GetAdjacentTiles(you.head);
-	
+
 	state.turns = 0;
-	
+
 	if (_.isEqual(you.head, state.target)) state.target = null;
 	if (state.target) {
 		state.move = Path.ApproachTarget(state.target, req.body);
 		return res.send({ move: state.move });
 	}
-	
+
 	if (you.health <= state.maxTravel) {
 		state.move = Path.ApproachTarget(Food.FindClosest({ you, board }), req.body);
 		return res.send({ move: state.move });
 	}
-	
+
 	if (Position.IsSafe(adjacent[state.move], { board, you })) {
 		return res.send({ move: state.move });
 	}
-	
+
 	state.move = Path.ApproachTarget(_.last(you.body), req.body);
 	if (Position.IsSafe(adjacent[state.move], { board, you })) {
 		return res.send({ move: state.move });
 	}
-	
+
 	const turn = { right: 'up', up: 'left', left: 'down', down: 'right' };
 	while (state.turns < 3 && !Position.IsSafe(adjacent[state.move], { board, you })) {
 		state.move = turn[state.move];
 		state.turns += 1;
 	}
-	
+
 	return res.send({ move: state.move });
 });
 
 app.post('/end', (req, res) => {
 	console.log('GAMEOVER', req.body);
-	console.log('-----\n');
 	return res.sendStatus(200);
 });
 
