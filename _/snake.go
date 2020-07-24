e700d8ec9edc179725dc574b5615067ec28a1c86
Jared Tyler <jared@jaredtyler.ca>
2020-07-23 23:49:03 -0400

    Fix property not being public for json decode purposes.

--- _/snake.go
+++ _/snake.go
@@ -4,16 +4,16 @@ type Snake struct {
 	ID       string `json:"id"`
 	Name     string `json:"name"`
 	Head     Cell   `json:"head"`
-	fullBody []Cell `json:"body"`
+	FullBody []Cell `json:"body"`
 	Length   int    `json:"length"`
 	Health   int    `json:"health"`
 }
 
 func (snake Snake) Body() []Cell {
-	return snake.fullBody[:len(snake.fullBody)-1]
+	return snake.FullBody[:len(snake.FullBody)-1]
 }
 
 // TODO: We could set Body to a slice of the original body, then grab the tail here using cap() instead of len()?
 func (snake Snake) Tail() Cell {
-	return snake.fullBody[len(snake.fullBody)-1]
+	return snake.FullBody[len(snake.FullBody)-1]
 }
