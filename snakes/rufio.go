3285f2b748a2029171f09ad5a6b4e2fcc59b6b33
Jared Tyler <jared@jaredtyler.ca>
2020-07-31 17:43:07 -0400

    Try not to run off the board if we can help it, even when welping out.

--- snakes/rufio.go
+++ snakes/rufio.go
@@ -71,6 +71,19 @@ func (me *Rufio) Move(context snek.Context) string {
 	}
 
 	// TODO: Should still prefer to pick a random adjacent empty cell before fully welping out.
+	nonWalls := make([]string, 0)
+	isContinueOpen := false
+	for dir, _ := range adjacent {
+		nonWalls = append(nonWalls, dir)
+		if dir == state.Move {
+			isContinueOpen = true
+			break
+		}
+	}
+
+	if !isContinueOpen {
+		state.Move = nonWalls[0]
+	}
 
 	utils.LogMove(context.Turn, state.Move, "welp 👋")
 	return state.Move
--- snakes/rufio/tactics/OnlyOneOption.go
+++ snakes/rufio/tactics/OnlyOneOption.go
@@ -2,11 +2,11 @@ package tactics
 
 import snek "github.com/JMTyler/battlesnake/_"
 
-type OnlyOneOption struct {}
+type OnlyOneOption struct{}
 
 func (_ OnlyOneOption) Run(context snek.Context, _ *snek.State) string {
 	adjacent := context.You.Head.GetAdjacentCells()
-	
+
 	options := make([]string, 0)
 	for dir, cell := range adjacent {
 		if !cell.IsDeadly(context) {
