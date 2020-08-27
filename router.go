499028e7b18c80bfcb11bb7f5691c4599996e589
Jared Tyler <jared@jaredtyler.ca>
2020-08-27 15:33:06 -0400

    Refactor router.

--- router.go
+++ router.go
@@ -20,7 +20,7 @@ var the_snakes = []snakes.SnakeService{
 	&snakes.Proxy{},
 }
 
-func handleRoute(route string, snake snakes.SnakeService, f func(snakes.SnakeService, http.ResponseWriter, *http.Request)) {
+func handleRoute(route string, snake snakes.SnakeService, f func(snakes.SnakeService, *snek.Context) []byte) {
 	http.HandleFunc(route, func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			// HACK: Would be simpler to use sentry.Recover() but it doesn't seem to work as expected.
@@ -41,6 +41,8 @@ func handleRoute(route string, snake snakes.SnakeService, f func(snakes.SnakeSer
 			}
 		}()
 
+		start := time.Now()
+
 		w.Header().Add("Access-Control-Allow-Origin", "*")
 		w.Header().Add("Access-Control-Allow-Headers", "*")
 		if r.Method == "OPTIONS" {
@@ -50,7 +52,44 @@ func handleRoute(route string, snake snakes.SnakeService, f func(snakes.SnakeSer
 			fmt.Println("[http]", r.Method, r.RequestURI)
 		}
 		w.Header().Add("Content-Type", "application/json")
-		f(snake, w, r)
+
+		var ctx *snek.Context
+		if r.Method == "POST" {
+			bytes, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				panic(err)
+			}
+
+			if err := json.Unmarshal(bytes, &ctx); err != nil {
+				panic(err)
+			}
+			ctx.Prepare()
+
+			sentry.ConfigureScope(func(scope *sentry.Scope) {
+				scope.SetRequest(r)
+				scope.SetUser(sentry.User{ID: ctx.Game.ID})
+				scope.SetTag("game", fmt.Sprintf("https://play.battlesnake.com/g/%s", ctx.Game.ID))
+				scope.SetTag("turn", fmt.Sprintf("%v", ctx.Turn))
+			})
+		}
+
+		if response := f(snake, ctx); response != nil {
+			w.Write(response)
+		}
+
+		duration := time.Now().Sub(start).Milliseconds()
+		if duration >= 400 {
+			// If request takes longer than 400ms, something is wrong.
+			sentry.WithScope(func(scope *sentry.Scope) {
+				scope.SetLevel(sentry.LevelWarning)
+				scope.SetTag("duration", fmt.Sprintf("%v", duration))
+				sentry.CaptureMessage("Move took unusually long to calculate.")
+			})
+		}
+
+		sentry.ConfigureScope(func(scope *sentry.Scope) {
+			scope.Clear()
+		})
 	})
 }
 
@@ -58,57 +97,21 @@ func handleRoute(route string, snake snakes.SnakeService, f func(snakes.SnakeSer
 func RouteSnakes() {
 	for _, snake := range the_snakes {
 		prefix := "/" + snake.GetName()
-		handleRoute(prefix+"/", snake, func(snake snakes.SnakeService, w http.ResponseWriter, r *http.Request) {
+		handleRoute(prefix+"/", snake, func(snake snakes.SnakeService, ctx *snek.Context) []byte {
 			info := snake.GetInfo()
 			info.APIVersion = "1"
 			info.Author = "JMTyler"
 
 			payload, _ := json.Marshal(info)
-			w.Write(payload)
+			return payload
 		})
 
-		handleRoute(prefix+"/start", snake, func(snake snakes.SnakeService, w http.ResponseWriter, r *http.Request) {
-			//			var bytes []byte
-			//			n, err := r.Body.Read(bytes)
-			bytes, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				panic(err)
-			}
-
-			var ctx *snek.Context
-			if err := json.Unmarshal(bytes, &ctx); err != nil {
-				panic(err)
-			}
-			ctx.Prepare()
-
-			sentry.ConfigureScope(func(scope *sentry.Scope) {
-				scope.SetRequest(r)
-				scope.SetUser(sentry.User{ID: ctx.Game.ID})
-			})
-
+		handleRoute(prefix+"/start", snake, func(snake snakes.SnakeService, ctx *snek.Context) []byte {
 			snake.StartGame(ctx)
-
-			sentry.ConfigureScope(func(scope *sentry.Scope) {
-				scope.Clear()
-			})
+			return nil
 		})
 
-		handleRoute(prefix+"/move", snake, func(snake snakes.SnakeService, w http.ResponseWriter, r *http.Request) {
-			bytes, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				panic(err)
-			}
-
-			var ctx *snek.Context
-			if err := json.Unmarshal(bytes, &ctx); err != nil {
-				panic(err)
-			}
-			ctx.Prepare()
-
-			sentry.ConfigureScope(func(scope *sentry.Scope) {
-				scope.SetRequest(r)
-				scope.SetUser(sentry.User{ID: ctx.Game.ID})
-			})
+		handleRoute(prefix+"/move", snake, func(snake snakes.SnakeService, ctx *snek.Context) []byte {
 
 			frame := db.NewFrame(ctx)
 			if !ctx.Game.Dev {
@@ -118,17 +121,6 @@ func RouteSnakes() {
 			start := time.Now()
 			move := snake.Move(ctx)
 			duration := time.Now().Sub(start).Microseconds()
-
-			// If move takes longer than 400ms, something is wrong.
-			if duration >= 400000 {
-				sentry.WithScope(func(scope *sentry.Scope) {
-					scope.SetLevel(sentry.LevelWarning)
-					scope.SetTag("game", fmt.Sprintf("https://play.battlesnake.com/g/%s", frame.GameID))
-					scope.SetTag("turn", fmt.Sprintf("%v", frame.Turn))
-
-					sentry.CaptureMessage("Move took unusually long to calculate.")
-				})
-			}
 
 			if !ctx.Game.Dev {
 				frame.Update(move, duration)
@@ -140,39 +132,17 @@ func RouteSnakes() {
 			if err != nil {
 				panic(err)
 			}
-			w.Write(payload)
-
-			sentry.ConfigureScope(func(scope *sentry.Scope) {
-				scope.Clear()
-			})
+			return payload
 		})
 
-		handleRoute(prefix+"/end", snake, func(snake snakes.SnakeService, w http.ResponseWriter, r *http.Request) {
-			bytes, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				panic(err)
-			}
-
-			var ctx *snek.Context
-			if err := json.Unmarshal(bytes, &ctx); err != nil {
-				panic(err)
-			}
-			ctx.Prepare()
-
-			sentry.ConfigureScope(func(scope *sentry.Scope) {
-				scope.SetRequest(r)
-				scope.SetUser(sentry.User{ID: ctx.Game.ID})
-			})
-
+		handleRoute(prefix+"/end", snake, func(snake snakes.SnakeService, ctx *snek.Context) []byte {
 			snake.EndGame(ctx)
 
 			if !ctx.Game.Dev {
 				db.PruneGames()
 			}
 
-			sentry.ConfigureScope(func(scope *sentry.Scope) {
-				scope.Clear()
-			})
+			return nil
 		})
 	}
 }
