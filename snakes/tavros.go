f08232462fe0a724e4e288f50c986b5cf98f4e2b
Jared Tyler <jared@jaredtyler.ca>
2020-08-31 03:17:53 -0400

    Tavros, stop charging forward blindly any time you eat.

--- snakes/tavros.go
+++ snakes/tavros.go
@@ -26,18 +26,31 @@ func (me *Tavros) Move(ctx *snek.Context) string {
 
 	move := ""
 
+	// Grab food if you're close to it, or if you're super hungry.
 	if food := ctx.You.Head.FindClosestTarget(ctx.Board.Food); food != nil {
 		if ctx.You.Health <= 20 || ctx.You.Head.GetDistance(food) <= 2 {
 			move = ctx.You.Head.ApproachTarget(food, ctx)
 		}
 	}
 
+	// Chase your tail.
 	if move == "" {
 		move = ctx.You.Head.ApproachTarget(ctx.You.Tail(), ctx)
 	}
 
 	if move == "" {
 		move = state.Move
+	}
+
+	// Don't ever move into a deadly space, even if the alternative is random.
+	target := ctx.You.Head.Adjacent(move)
+	if target == nil || target.IsDeadly(ctx) {
+		for dir, cell := range ctx.You.Head.GetAdjacentCells() {
+			if !cell.IsDeadly(ctx) {
+				move = dir
+				break
+			}
+		}
 	}
 
 	state.Move = move
