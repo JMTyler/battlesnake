a3e21895d7588a80263b7c2a445f78bfa662359d
Jared Tyler <jared@jaredtyler.ca>
2020-08-31 16:43:56 -0400

    If you're already within the centre area, don't try to go there.

--- snakes/rufio/tactics/GoCentre.go
+++ snakes/rufio/tactics/GoCentre.go
@@ -21,6 +21,14 @@ func (opts GoCentre) Run(ctx *snek.Context, _ *snek.State) string {
 	leftEdge := (ctx.Board.Width - opts.Width) / 2
 	bottomEdge := (ctx.Board.Height - opts.Height) / 2
 
+	// If you're already within the centre area, move on.
+	you := ctx.You.Head
+	if you.X >= leftEdge && you.X < leftEdge+opts.Width {
+		if you.Y >= bottomEdge && you.Y < bottomEdge+opts.Height {
+			return ""
+		}
+	}
+
 	var centreCells []*snek.Cell
 	for x := leftEdge; x < leftEdge+opts.Width; x++ {
 		for y := bottomEdge; y < bottomEdge+opts.Height; y++ {
